9cda54e24acdf9fa9bbd439d65f59c1224db8332
BrunoMg0 <45740703+BrunoMg0@users.noreply.github.com>
2018-12-11 19:42:43 -0200

    Add files via upload

---
+++ Controller/ProdutoController.php
@@ -0,0 +1,76 @@
+<?php
+
+ini_set('display_errors', true);
+require_once $_SERVER['DOCUMENT_ROOT'] . "/Dao/ProdutoDao.php";
+
+
+   
+switch ($_REQUEST['acao']) {
+    case 'salvarProduto':
+        if(empty($_REQUEST['nomeprod']) ){
+            header ('location: ../cadastroProduto.php?erro=Nome Obrigatório ');
+            break;
+        }
+        
+        if (empty($_REQUEST['caminhoimg'])){
+            header ('location: ../cadastroProduto.php?erro=Digite o caminho de uma imagem');
+            break;
+        }
+        if (empty($_REQUEST['legendaprod'])||empty($_REQUEST['categoria'])){
+            header ('location: ../cadastroProduto.php?erro=Preencha todos os campos!');
+            break;
+        }
+ 
+        
+        $dao = new ProdutoDao();
+        $inseriu=$dao->inserirProduto($_REQUEST['nomeprod'], $_REQUEST['caminhoimg'], $_REQUEST['legendaprod'], $_REQUEST['categoria']);
+        
+        if ($inseriu){
+            echo '<script>alert(" '."Produto cadastrado!".'  ");</script>'; 
+            echo "<script>window.location = '../cadastroProduto.php';</script>"; 
+        }
+  
+        break;
+        
+    case 'alterarProduto':
+        if(empty($_REQUEST['nomeprod']) ){
+            header ('location: ../cadastroProduto.php?erro=Nome Obrigatório ');
+            break;
+        }
+        
+        if (empty($_REQUEST['caminhoimg'])){
+            header ('location: ../cadastroProduto.php?erro=Digite o caminho de uma imagem');
+            break;
+        }
+        if (empty($_REQUEST['legendaprod'])||empty($_REQUEST['categoria'])){
+            header ('location: ../cadastroProduto.php?erro=Preencha todos os campos!');
+            break;
+        }
+        $dao = new ProdutoDao();
+        $alterou=$dao->alterarProduto($_REQUEST['idProd'],$_REQUEST['nomeprod'], $_REQUEST['caminhoimg'], $_REQUEST['legendaprod'], $_REQUEST['categoria']);
+        
+        if ($alterou){
+            echo '<script>alert(" '."Produto alterado!".'  ");</script>'; 
+            echo "<script>window.location = '../alteraProduto.php';</script>"; 
+        }
+  
+        break;
+        
+    case 'deletarProduto':
+        $dao = new ProdutoDao();
+        $excluiu=$dao->deletarProduto($_REQUEST['idProd']);
+        
+        if ($excluiu){
+            echo '<script>alert(" '."Produto excluído!".'  ");</script>'; 
+            echo "<script>window.location = '../excluiProduto.php';</script>"; 
+        }
+  
+        break;
+        
+        
+    default:
+        echo 'Ação não identificada';
+        break;
+}
+    
+?>
