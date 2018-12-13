e8b15378927fde72923a6186fcad6537892124de
BrunoMg0 <45740703+BrunoMg0@users.noreply.github.com>
2018-12-13 15:26:01 -0200

    Add files via upload

--- Controller/ProdutoController.php
+++ Controller/ProdutoController.php
@@ -4,73 +4,91 @@ ini_set('display_errors', true);
 require_once $_SERVER['DOCUMENT_ROOT'] . "/Dao/ProdutoDao.php";
 
 
-   
+
 switch ($_REQUEST['acao']) {
     case 'salvarProduto':
-        if(empty($_REQUEST['nomeprod']) ){
-            header ('location: ../cadastroProduto.php?erro=Nome Obrigatório ');
+        $img = $_FILES['img']['name'];
+        $temp = $_FILES['img']['tmp_name'];
+
+        if (empty($_REQUEST['nomeprod'])) {
+            header('location: ../cadastroProduto.php?erro=Nome Obrigatório ');
             break;
         }
-        
-        if (empty($_REQUEST['caminhoimg'])){
-            header ('location: ../cadastroProduto.php?erro=Digite o caminho de uma imagem');
+
+        if (empty($img)) {
+            header('location: ../cadastroProduto.php?erro=Insira uma imagem');
             break;
         }
-        if (empty($_REQUEST['legendaprod'])||empty($_REQUEST['categoria'])){
-            header ('location: ../cadastroProduto.php?erro=Preencha todos os campos!');
+        if (empty($_REQUEST['legendaprod']) || empty($_REQUEST['categoria'])) {
+            header('location: ../cadastroProduto.php?erro=Preencha todos os campos!');
             break;
         }
- 
-        
-        $dao = new ProdutoDao();
-        $inseriu=$dao->inserirProduto($_REQUEST['nomeprod'], $_REQUEST['caminhoimg'], $_REQUEST['legendaprod'], $_REQUEST['categoria']);
-        
-        if ($inseriu){
-            echo '<script>alert(" '."Produto cadastrado!".'  ");</script>'; 
-            echo "<script>window.location = '../cadastroProduto.php';</script>"; 
+
+
+
+        if (move_uploaded_file($temp, '../imgsl/' . $img)) {
+            $dao = new ProdutoDao();
+            $inseriu = $dao->inserirProduto($_REQUEST['nomeprod'], "imgsl/" . $img, $_REQUEST['legendaprod'], $_REQUEST['categoria']);
+
+            if ($inseriu) {
+                echo '<script>alert(" ' . "Produto cadastrado!" . '  ");</script>';
+                echo "<script>window.location = '../cadastroProduto.php';</script>";
+            }
+        } else {
+            echo '<script>alert(" ' . "Erro ao mover imagem!" . '  ");</script>';
         }
-  
+
+
+
+
         break;
-        
+
     case 'alterarProduto':
-        if(empty($_REQUEST['nomeprod']) ){
-            header ('location: ../cadastroProduto.php?erro=Nome Obrigatório ');
+        $img = $_FILES['img']['name'];
+        $temp = $_FILES['img']['tmp_name'];
+
+        if (empty($_REQUEST['nomeprod'])) {
+            header('location: ../alteraProduto.php?erro=Nome Obrigatório ');
             break;
         }
-        
-        if (empty($_REQUEST['caminhoimg'])){
-            header ('location: ../cadastroProduto.php?erro=Digite o caminho de uma imagem');
+
+        if (empty($img)) {
+            header('location: ../alteraProduto.php?erro=Insira uma imagem');
             break;
         }
-        if (empty($_REQUEST['legendaprod'])||empty($_REQUEST['categoria'])){
-            header ('location: ../cadastroProduto.php?erro=Preencha todos os campos!');
+        if (empty($_REQUEST['legendaprod']) || empty($_REQUEST['categoria'])) {
+            header('location: ../alteraProduto.php?erro=Preencha todos os campos!');
             break;
         }
-        $dao = new ProdutoDao();
-        $alterou=$dao->alterarProduto($_REQUEST['idProd'],$_REQUEST['nomeprod'], $_REQUEST['caminhoimg'], $_REQUEST['legendaprod'], $_REQUEST['categoria']);
-        
-        if ($alterou){
-            echo '<script>alert(" '."Produto alterado!".'  ");</script>'; 
-            echo "<script>window.location = '../alteraProduto.php';</script>"; 
+
+        if (move_uploaded_file($temp, '../imgsl/' . $img)) {
+            $dao = new ProdutoDao();
+            $alterou = $dao->alterarProduto($_REQUEST['idProd'], $_REQUEST['nomeprod'], "imgsl/" . $img, $_REQUEST['legendaprod'], $_REQUEST['categoria']);
+
+            if ($alterou) {
+                echo '<script>alert(" ' . "Produto alterado!" . '  ");</script>';
+                echo "<script>window.location = '../alteraProduto.php';</script>";
+            }
+        } else {
+            echo '<script>alert(" ' . "Erro ao mover imagem!" . '  ");</script>';
         }
-  
+
         break;
-        
+
     case 'deletarProduto':
         $dao = new ProdutoDao();
-        $excluiu=$dao->deletarProduto($_REQUEST['idProd']);
-        
-        if ($excluiu){
-            echo '<script>alert(" '."Produto excluído!".'  ");</script>'; 
-            echo "<script>window.location = '../excluiProduto.php';</script>"; 
+        $excluiu = $dao->deletarProduto($_REQUEST['idProd']);
+
+        if ($excluiu) {
+            echo '<script>alert(" ' . "Produto excluído!" . '  ");</script>';
+            echo "<script>window.location = '../excluiProduto.php';</script>";
         }
-  
+
         break;
-        
-        
+
+
     default:
         echo 'Ação não identificada';
         break;
 }
-    
 ?>
